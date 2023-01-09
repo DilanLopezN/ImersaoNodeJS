673792236370020ef8005734014661eec39cdeed
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-09 17:06:59 -0300

    test perfomace with for foroff forin

--- ApiManipulation/for-forin-forof.js
+++ ApiManipulation/for-forin-forof.js
@@ -4,12 +4,26 @@ async function main() {
   try {
     const result = await getPeople('a')
     const names = []
+
     console.time('tempo com for')
-    for (let i = 0; i <= result.results.length - 1; i++) {
+    /*for (let i = 0; i <= result.results.length - 1; i++) {
       const people = result.results[i]
       names.push(people.name)
     }
     console.timeEnd('tempo com for')
+
+    console.time('tempo com forIn')
+    for (let i in result.results) {
+      const people = result.results[i]
+      names.push(people.name)
+    }
+    console.timeEnd('tempo com forIn')
+    */
+    console.time('tempo com forOff')
+    for (pessoas of result.results) {
+      names.push(pessoas.name)
+    }
+    console.timeEnd('tempo com forOff')
 
     console.log('names', names)
   } catch (error) {
