590a19d2b554ab9107ef18e2d6f2098563efaa47
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-10 15:10:43 -0300

    map and filter api

--- ApiManipulation/map.js
+++ ApiManipulation/map.js
@@ -1,11 +1,12 @@
 import { getPeople } from './service.js'
 async function main() {
   try {
-    const results = await getPeople('a')
-    const names = []
-    results.results.forEach(item => {
-      names.push(item.name)
+    const { results } = await getPeople('a')
+    const larsFamily = results.filter(item => {
+      const result = item.name.toLowerCase().indexOf('lars') !== -1
+      return result
     })
+    const names = larsFamily.map(person => person.name)
     console.log(names)
   } catch (error) {
     console.error(error)
