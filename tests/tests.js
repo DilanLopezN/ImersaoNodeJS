38830d68710511de0a37f78c7d29c261d5bb53bf
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-10 16:52:14 -0300

    fix erros and speed up test

--- tests/tests.js
+++ tests/tests.js
@@ -1,14 +1,48 @@
 import { obterDados } from './service.js'
 import assert from 'node:assert'
+import nock from 'nock'
 
-describe('Star Wars', function () {
-  it('deve buscar r2d2 com formato correto', async () => {
-    const expected = [{ nome: 'RD-D2', peso: '96' }]
-    const nomeBase = `r2-d2`
+try {
+  describe('Star Wars', function () {
+    this.beforeAll(() => {
+      const response = {
+        count: 1,
+        next: null,
+        previous: null,
+        results: [
+          {
+            name: 'R2-D2',
+            height: '96',
+            mass: '32',
+            hair_color: 'n/a',
+            skin_color: 'white, blue',
+            eye_color: 'red',
+            birth_year: '33BBY',
+            gender: 'n/a',
+            homeworld: 'https://swapi.dev/api/planets/8/',
+            vehicles: [],
+            starships: [],
+            created: '2014-12-10T15:11:50.376000Z',
+            edited: '2014-12-20T21:17:50.311000Z',
+            url: 'https://swapi.dev/api/people/3/'
+          }
+        ]
+      }
 
-    const resultado = await obterDados(nomeBase)
-    console.log(resultado)
+      nock('https://swapi.dev/api/people')
+        .get('/?search=r2-d2&format=json')
+        .reply(200, response)
+    })
+    it('deve buscar r2d2 com formato correto', async () => {
+      const expected = [{ nome: 'R2-D2', peso: '96' }]
+      const nomeBase = `r2-d2`
 
-    assert.deepEqual(resultado, expected)
-  })
-})
+      const resultado = await obterDados(nomeBase)
+      console.log(resultado)
+
+      assert.deepEqual(resultado, expected)
+    })
+  }).timeout(10000)
+} catch (error) {
+  console.log(error)
+}
