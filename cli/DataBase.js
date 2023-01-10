d560b0bf5fbb956c7b112c198379d4d038532ab6
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-10 19:58:52 -0300

    others test and database read and filter

--- cli/DataBase.js
+++ cli/DataBase.js
@@ -1,20 +1,33 @@
-import { readFile } from 'fs'
-import { promisify } from 'util'
-const readFileAsync = promisify(readFile)
+import { readFileSync, writeFileSync } from 'fs'
+
 class Database {
   constructor() {
     this.NOME_ARQUIVO = 'herois.json'
   }
   async obterDadosArquivo() {
-    const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
+    const arquivo = readFileSync(this.NOME_ARQUIVO)
     return JSON.parse(arquivo.toString())
   }
-  async escreverArquivo() {}
+  async escreverArquivo(dados) {
+    writeFileSync(this.NOME_ARQUIVO, JSON.stringify(dados))
+    return true
+  }
+
+  async cadastrar(heroi) {
+    const dados = await this.obterDadosArquivo()
+    const id = heroi.id <= 2 ? heroi.id : Date.now()
+    const heroisComId = {
+      id,
+      ...heroi
+    }
+    const dadosFinal = [...dados, heroisComId]
+    const resultado = await this.escreverArquivo(dadosFinal)
+    return resultado
+  }
 
   async listar(id) {
     const dados = await this.obterDadosArquivo()
-    const dadosFiltrados = dados.filter(item => (id ? item.id === id : true))
-    return dadosFiltrados
+    return dados.filter(item => (id ? item.id == id : true))
   }
 }
 
--- cli/test.js
+++ cli/test.js
@@ -5,14 +5,14 @@ const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'speed', id: 1 }
 describe('Suite de manipulação de Herois', () => {
   it('deve pesquisar heroi usando arquivo', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR
-    const resultado = await Database.listar(expected.id)
-    ok(resultado, expected)
+    const [resultado] = await Database.listar(expected.id)
+    deepEqual(resultado, expected)
   })
 
-  /*
   it('deve cadastrar um heroi, usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR
-    ok(null, expected)
+    const resultado = await Database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    const [atual] = await Database.listar(DEFAULT_ITEM_CADASTRAR.id)
+    deepEqual(atual, expected)
   })
-  */
 })
